3156198c4d3a311cf7a498083c5541c23ed76eca
david <david.ortega.pajaro1@gmail.com>
2024-03-11 19:03:23 -0500

    antes del jws

--- back/routes/api.php
+++ back/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\PaginaController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/roles', RoleController::class);
+
+Route::resource('/usuarios', UsuarioController::class);
+
+Route::resource('/paginas', PaginaController::class);
